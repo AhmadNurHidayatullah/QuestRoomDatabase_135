304eba30fc2b3344ecf8d31c6b4fbd3a297cc549
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-12-16 12:59:27 +0700

    menambahkan objek destinasi untuk rute navigasi insert, home, detail, dan update dengan parameter NIM

--- app/src/main/java/com/example/appmahasiswa/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/appmahasiswa/ui/navigation/AlamatNavigasi.kt
@@ -3,3 +3,23 @@ package com.example.appmahasiswa.ui.navigation
 interface AlamatNavigasi {
     val route: String
 }
+
+object DestinasiInsert : AlamatNavigasi {
+    override val route: String = "insert"
+}
+
+object DestinasiHome : AlamatNavigasi {
+    override val route = "home"
+}
+
+object DestinasiDetail : AlamatNavigasi {
+    override val route = "detail"
+    const val NIM = "nim"
+    val routesWithArg = "$route/{$NIM}"
+}
+
+object DestinasiUpdate : AlamatNavigasi {
+    override val route = "update"
+    const val NIM = "nim"
+    val routesWithArg = "$route/{$NIM}"
+}
