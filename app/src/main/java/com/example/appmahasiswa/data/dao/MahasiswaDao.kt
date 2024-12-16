46ea61356614a93404a9a7ee9d39c95677c518b1
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-12-16 12:47:54 +0700

    menambahkan metode mengambil mahasiswa berdasarkan nim, memperbarui data mahasiswa ,dan menghapus

--- app/src/main/java/com/example/appmahasiswa/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/appmahasiswa/data/dao/MahasiswaDao.kt
@@ -1,8 +1,10 @@
 package com.example.appmahasiswa.data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.example.appmahasiswa.data.entity.Mahasiswa
 import kotlinx.coroutines.flow.Flow
 
@@ -11,6 +13,16 @@ import kotlinx.coroutines.flow.Flow
 interface MahasiswaDao {
     @Query("select * from mahasiswa")
     fun getAllMahasiswa() : Flow<List<Mahasiswa>>
+
+    @Query("SELECT * FROM mahasiswa WHERE nim = :nim")
+    fun getMahasiswa(nim: String): Flow<Mahasiswa>
+
     @Insert
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+
+    @Update
+    suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
+
+    @Delete
+    suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
 }
